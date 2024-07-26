f3d868f1916e6a0b4546c04e1b8718f73336d638
Andre Wilkinson <andre_w3@yahoo.com>
2024-07-25 21:29:59 -0400

    writing first model

--- fullstackBarCode/api/models.py
+++ fullstackBarCode/api/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+import string
+import random
 
+def generate_unique_code():
+    length = 6
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
 # Create your models here.
 class Room(models.Model):
     #code === DCPI
@@ -7,6 +16,6 @@ class Room(models.Model):
     product = models.CharField(max_length=50, unique=True)
     can_add_photo = models.BooleanField(null=False, default=True)
     vote_as_damaged = models.ImageField(null=False, default=1)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 

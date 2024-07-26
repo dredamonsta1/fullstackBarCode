55a1e3378721b1a9af58f7f8b8e5fdf77e732280
Andre Wilkinson <andre_w3@yahoo.com>
2024-07-25 21:46:13 -0400

    fixed migration errror

--- fullstackBarCode/api/models.py
+++ fullstackBarCode/api/models.py
@@ -12,7 +12,7 @@ def generate_unique_code():
 # Create your models here.
 class Room(models.Model):
     #code === DCPI
-    code = models.charField(max_length=20, default="", unique=True)
+    code = models.CharField(max_length=20, default="", unique=True)
     product = models.CharField(max_length=50, unique=True)
     can_add_photo = models.BooleanField(null=False, default=True)
     vote_as_damaged = models.ImageField(null=False, default=1)

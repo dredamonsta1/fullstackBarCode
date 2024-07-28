55c0e15eff9ecf6de834c0f051a884e73654f6dd
Andre Wilkinson <andre_w3@yahoo.com>
2024-07-27 20:30:44 -0400

    serializer class in view

--- fullstackBarCode/api/views.py
+++ fullstackBarCode/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 class RomeView(generics.CreateAPIView):

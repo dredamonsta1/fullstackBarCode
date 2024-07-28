8bee17d9dedc516b85f4609bf5f60a7e7f2d0caa
Andre Wilkinson <andre_w3@yahoo.com>
2024-07-27 20:22:54 -0400

    setting up serializer

--- fullstackBarCode/api/serializers.py
+++ fullstackBarCode/api/serializers.py
@@ -4,4 +4,4 @@ from .model import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('')
+        fields = ('id', 'code', 'product', 'can_add_photo', 'vote_as_damaged', 'created_at')
